256493eeb4ec8a2765e85f1b947a040d8c002ea5
gop290 <abdulhafiz091181@gmail.com>
2026-09-19 16:02:17 +0300

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  base: "/project-one/",
+
   test: {
     environment: "jsdom",
     setupFiles: "./src/setupTests.js"
